07f495d4771d881c7f4d3551369bf26f190193cd
LevonAr <levonrm10@gmail.com>
2021-05-12 18:52:15 -0700

    ADD climbingStairsBF.js

---
+++ 70-climbing-stairs/climbingStairsBF.js
@@ -0,0 +1,13 @@
+// Created: 5/12/21
+// To Solve: Leetcode 70: Climbing Stairs
+
+function climbingStairsBF(n) {
+  if (n == 0) {
+    return 0;
+  }
+
+  if (n == 1) {
+    return 1;
+  }
+  return climbingStairsBF(n - 1) + climbingStairsBF(n - 2);
+}
