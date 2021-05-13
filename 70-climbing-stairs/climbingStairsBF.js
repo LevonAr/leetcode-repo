1a6344f396a5972b5fc86de8007167c32ea3f01a
LevonAr <levonrm10@gmail.com>
2021-05-12 19:09:29 -0700

    UPDATE climbingStairsBF.js

--- 70-climbing-stairs/climbingStairsBF.js
+++ 70-climbing-stairs/climbingStairsBF.js
@@ -1,17 +1,28 @@
-// Created: 5/12/21
-// To Solve: Leetcode 70: Climbing Stairs
-
+/**
+ * Created: 5/12/21
+ * To Solve: Leetcode 70: Climbing Stairs
+ * (Brute Force Solution)
+ *
+ * @param {number} n - Number of stairs to climb on.
+ * @returns {number} - Number of ways to climb a staircase.
+ */
 function climbingStairsBF(n) {
   if (n <= 0) {
+    // There is no way to go down - you climb the stairs only upwards.
+    // Also if you're standing on the ground floor that you don't need to do any further steps.
     return 0;
   }
 
   if (n == 1) {
+    // There is only one way to go to the first step.
     return 1;
   }
 
   if (n == 2) {
+    // There are two ways to get to the second steps: (1 + 1) or (2).
     return 2;
   }
+  // Sum up how many steps we need to take after doing one step up with the number of
+  // steps we need to take after doing two steps up.
   return climbingStairsBF(n - 1) + climbingStairsBF(n - 2);
 }
