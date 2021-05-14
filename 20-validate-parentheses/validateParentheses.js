45b75133a652a97ea32d3bcc39405a4a9dadc4ac
LevonAr <levonrm10@gmail.com>
2021-05-14 10:53:58 -0700

    UPDATE 20. validate parentheses

--- 20-validate-parentheses/validateParentheses.js
+++ 20-validate-parentheses/validateParentheses.js
@@ -1,8 +1,8 @@
 function validateParenthese(string) {
-  // initialize stack as array
+  // Initialize stack as array
   let stack = [];
 
-  // make hashtable with close-open key-value pairs
+  // Make hashtable with close-open key-value pairs. This keeps code clean and makes adding more types of parenthesis easier
   let hashTable = new Map([
     [")", "("],
     ["}", "{"],
@@ -10,15 +10,22 @@ function validateParenthese(string) {
   ]);
 
   for (let i = 0; i < string.length; i++) {
+    // If the character is an closing bracket
     if (hashTable.has(string[i])) {
+      // Pop the topmost element from the stack, if it is non empty otherwise assign dummy "#"
       let topElement = stack.length > 0 ? stack.pop() : "#";
+      // If the mapping for the opening bracket in our hash and the top element of the stack don't match, return False
       if (topElement !== hashTable.get(string[i])) {
         return false;
       }
-    } else {
+    }
+    // If we have an opening bracket, simply push it onto the stack.
+    else {
       stack.push(string[i]);
     }
   }
+
+  //In the end, if the stack is empty, then we have a valid expression.
   let returnValue = stack.length == 0 ? true : false;
   return returnValue;
 }
