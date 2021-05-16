2a6e7e850ab67688ad40f9c822b616ab97146c7a
LevonAr <levonrm10@gmail.com>
2021-05-16 15:21:29 -0700

    ADD reSolve1_searchInsertPosition : inCorrect within 25 mins

---
+++ 35-search-insert-position/resolve/reSolve1_searchInsertPosition.js
@@ -0,0 +1,19 @@
+// TEST CASE : nums = [1,3,5,6]; target = 5
+// binary search
+
+var searchInsert = function (nums, target) {
+  let startIndex = 0;
+  let endIndex = nums.length - 1;
+  while (startIndex < endIndex) {
+    // issue here v
+    const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+    if (target == nums[middleIndex]) {
+      return middleIndex;
+    } else if (target < nums[middleIndex]) {
+      endIndex = middleIndex;
+    } else if (target > nums[middleIndex]) {
+      startIndex = middleIndex + 1;
+    }
+  }
+  return startIndex;
+};
