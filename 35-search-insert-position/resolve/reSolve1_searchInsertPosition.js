02fe4a3426315e1730eb6e78f9e6367163b4b943
LevonAr <levonrm10@gmail.com>
2021-05-16 15:29:52 -0700

    UPDATE reSolve1_searchInsertPosition : Corrected

--- 35-search-insert-position/resolve/reSolve1_searchInsertPosition.js
+++ 35-search-insert-position/resolve/reSolve1_searchInsertPosition.js
@@ -4,13 +4,12 @@
 var searchInsert = function (nums, target) {
   let startIndex = 0;
   let endIndex = nums.length - 1;
-  while (startIndex < endIndex) {
-    // issue here v
+  while (startIndex <= endIndex) {
     const middleIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
     if (target == nums[middleIndex]) {
       return middleIndex;
     } else if (target < nums[middleIndex]) {
-      endIndex = middleIndex;
+      endIndex = middleIndex - 1;
     } else if (target > nums[middleIndex]) {
       startIndex = middleIndex + 1;
     }
