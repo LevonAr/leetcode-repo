dbcae3539aaadd07daedcaf3ddf9de9f0a8f44df
LevonAr <levonrm10@gmail.com>
2021-05-16 19:14:20 -0700

    ADD reSolve1_bestTimeToBuyAndSellStock .js : correct within 15 mins

---
+++ 121-best-time-to-buy-and-sell-stock/re-solve/reSolve1_bestTimeToBuyAndSellStock.js
@@ -0,0 +1,15 @@
+// Big O
+// Time: O(n)
+// Space: O(1)
+//                                v
+// TEST CASE: prices = [7,1,5,3,6,4]
+function maxProfit(prices) {
+  let maxiProfit = 0;
+  let minPrice;
+  for (let i = 0; i < prices.length; i++) {
+    minPrice =
+      minPrice == undefined ? prices[i] : Math.min(prices[i], minPrice);
+    maxiProfit = Math.max(prices[i] - minPrice, maxiProfit);
+  }
+  return maxiProfit;
+}
