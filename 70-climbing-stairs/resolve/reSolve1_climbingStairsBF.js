aa9a4a4a3b82acf3f4c3887af1c31874c42341e8
LevonAr <levonrm10@gmail.com>
2021-05-16 18:10:31 -0700

    ADD reSolve1_climbingStairsBF.js : correct within 20 mins

---
+++ 70-climbing-stairs/resolve/reSolve1_climbingStairsBF.js
@@ -0,0 +1,17 @@
+// Brute Force Solution
+// Big O
+// Space: O()?
+// Time:O(1)
+
+var climbStairs = function (n) {
+  if (n == 0) {
+    return 0;
+  }
+  if (n == 1) {
+    return 1;
+  }
+  if (n == 2) {
+    return 2;
+  }
+  return climbStairs(n - 1) + climbStairs(n - 2);
+};
