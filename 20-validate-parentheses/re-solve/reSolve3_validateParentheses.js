799e15720de1543252eda3034f72eb307806f037
LevonAr <levonrm10@gmail.com>
2021-12-30 20:24:49 -0800

    reSolve3_validateParentheses: correct solution

--- 20-validate-parentheses/re-solve/reSolve3_validateParentheses.js
+++ 20-validate-parentheses/re-solve/reSolve3_validateParentheses.js
@@ -4,22 +4,22 @@
  */
 
 // (){(}
-var isValid = function(s) {
-    const hashMap = {
-        '(' : ')',
-        '{' : '}',
-        '[' : ']',
-    }
-    let currentOpen = [] 
-    for(let i = 0; i<s.length; i++){
-        if (closed && doesnt match currentOpen)
-            return false
-        else if closed && match currentOpen
-            pop currentOpen STACK
-        if open 
-            add to currentOpen STACK
-    }
-    if currentOpen.length == 0 
-        return true
-    else return false
+var isValid = function (s) {
+  const hashMap = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  let currentOpen = [];
+  for (let i = 0; i < s.length; i++) {
+    if (!(s[i] in hashMap)) {
+      let topElement = currentOpen.pop();
+      if (hashMap[topElement] !== s[i]) return false;
+    } else currentOpen.push(s[i]);
+  }
+  if (currentOpen.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
